1407eb58cadc7af1ed7fcc7b0d8f9eaab40c1590
Abhishek <80732845+Lostman76@users.noreply.github.com>
2024-07-09 11:22:07 +0530

    Keys.py

--- Keys.py
+++ Keys.py
@@ -1,4 +1,4 @@
-api = "4lZQ3iP9pdlkPsDvMe"
-secret = "MrzS84TnFlsQtLh6BIyXITQd1Mo9cec2DFXH"
+api = "Api-Key"
+secret = "Your Secret Key"
 accountType = "CONTRACT"
-Coin_api = "d7484cf7-b165-4323-bf88-78cb78491a03"
+Coin_api = "Coin-Api"
